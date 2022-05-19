b4bfb4e8281f8ba71a9bdb4bf7e3ce29953e8a90
Filip Jerga <jerga99@gmail.com>
2022-05-19 09:58:41 +0100

    Assignment State

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -3,20 +3,22 @@ import { useState } from "react";
 import "./Counter.css";
 
 function Counter() {
-  const [count, setCount] = useState(100);
+  const [count, setCount] = useState(0);
   
-  const increment = () => {
-    setCount(count + 1);
+  const increment = (inc) => {
+    setCount(count + inc);
   }
 
   return (
     <div className="counter-container">
       <div>{count}</div>
 
-      <button onClick={increment}>
+      <button onClick={() => increment(+100)}>
         Increment
       </button>
-
+      <button onClick={() => increment(-50)}>
+        Decrement
+      </button>
     </div>
   )
 }
