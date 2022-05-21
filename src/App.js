9b2e5b4fb6b7284774755cd9e081b432080ea0cb
Filip Jerga <jerga99@gmail.com>
2022-05-21 20:10:32 +0100

    Columns Improvements

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 
 
 import { ProductPage } from "./components/ProductPage";
-import { ProductItem } from "./components/ProductItem";
-import { useEffect, useState } from "react";
-import { fetchProducts } from "./utils/fetchProducts";
 import Navbar from "./components/Navbar";
 
 export default function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const _products = await fetchProducts();
-      setProducts(_products);
-    }
-
-    fetchData();
-  }, [])
-
-  
-  // JSX!
   return (
-    <>
+    <div className="container">
       <Navbar />
-      <ProductPage>
-        { products.map((product) => 
-          <ProductItem 
-            key={product.image} 
-            product={product}
-          />
-        )}
-      </ProductPage>
-    </>
+      <ProductPage />
+    </div>
   )
 }
--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,13 +1,32 @@
+import { useEffect, useState } from "react";
+import { ProductItem } from "./ProductItem";
+import { fetchProducts } from "../utils/fetchProducts";
 
-export function ProductPage({children}) {
+export function ProductPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const _products = await fetchProducts();
+      setProducts(_products);
+    }
+
+    fetchData();
+  }, [])
 
   return (
     <div className="page">
-      <div className="container">
-        <h1>New Products</h1>
-        <div className="product-grid">
-          {children}
-        </div>
+      <div className="columns is-multiline">
+        { products.map((product) => 
+          <div 
+            className="column is-4"
+            key={product.image} 
+          >
+            <ProductItem 
+              product={product}
+            />
+          </div>
+        )}
       </div>
     </div>
   )
